e950db4b0f562bc15f820c348c565fa34caf6705
Guilherme Amoroso Romão <gromao@primeit.pt>
2022-08-21 18:53:34 +0100

    SelectionSort: code polish

--- src/co/gromao/practice/sort/selection/SelectionSort.java
+++ src/co/gromao/practice/sort/selection/SelectionSort.java
@@ -4,18 +4,16 @@ public class SelectionSort {
 
     public void sort(int [] array) {
         for (int i = 0; i < array.length; i++) {
-            int min = array[i];
-            int swapIndex = -1;
+            int minIndex = i;
+            int temp = array[i];
             for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < min) {
-                    min = array[j];
-                    swapIndex = j;
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
                 }
             }
-            if (min != array[i]) {
-                int temp = array[i];
-                array[i] = min;
-                array[swapIndex] = temp;
+            if (minIndex != i) {
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
             }
         }
     }
